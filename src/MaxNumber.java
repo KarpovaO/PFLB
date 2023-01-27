03c4c6a2d6b1037c15e0afc477cbc57d6cda2608
KarpovaO <grose@yandex.ru>
2023-01-28 00:04:56 +0300

    refresh gitignore

--- src/MaxNumber.java
+++ src/MaxNumber.java
@@ -1,6 +1,5 @@
 public class MaxNumber {
     public static void main(String[] args) {
-
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
