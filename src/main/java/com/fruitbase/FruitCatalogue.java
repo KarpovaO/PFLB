45587cf539d2d2916315807878086efec0418e0e
KarpovaO <grose@yandex.ru>
2023-03-01 21:25:56 +0300

    add megabankcorp + fruitbase

---
+++ src/main/java/com/fruitbase/FruitCatalogue.java
@@ -0,0 +1,21 @@
+package com.fruitbase;
+
+import com.fruitbase.fruits.*;
+
+public class FruitCatalogue {
+    Fruit[] fruit = {new Apple(), new Banana(), new Orange(), new Pineapple()};
+
+    FruitCatalogue() {
+
+    }
+
+    public Fruit findFruit(String fruitName) {
+        for (Fruit f : fruit) {
+            if (f.getName().equals(fruitName)) {
+                return f;
+            }
+
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/fruitbase/fruits/Apple.java
@@ -0,0 +1,11 @@
+package com.fruitbase.fruits;
+
+import java.math.BigDecimal;
+
+public class Apple extends Fruit {
+    public Apple() {
+        weight = 2;
+        price = BigDecimal.valueOf(150.0);
+        name = "Apple";
+    }
+}
